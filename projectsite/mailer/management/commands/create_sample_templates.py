b942749572d48656846990749bdba87d693d3ad3
n3mo1101 <rd.cruz1101@gmail.com>
2026-01-12 00:10:29 +0800

    Updated sample templates

--- projectsite/mailer/management/commands/create_sample_templates.py
+++ projectsite/mailer/management/commands/create_sample_templates.py
@@ -8,36 +8,19 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         templates = [
             {
-                'name': 'Template 1 - COR',
+                'name': 'Certificate of Registration',
                 'subject': 'Certificate of Registration',
                 'header_message': 'Congratulations!',
                 'body_content': '''On behalf of the College of Sciences, we are sending you your Certificate of Registration, which serves as your proof of enrollment.
 
 It is important that you keep this document as part of your record, as this is an official school document.
 
-If you have any questions or need additional copies of your certificate, please don't hesitate to contact us.
-
-<h4>Welcome to the College of Sciences!</h4>''',
+<h3 style="color: initial; letter-spacing: 1px;">Welcome to the College of Sciences!</h3>''',
                 'is_predefined': True
             },
             {
-                'name': 'Template 2 - COA',
-                'subject': 'Certificate of Achievement',
-                'header_message': 'Outstanding Achievement!',
-                'body_content': '''We are delighted to recognize your exceptional performance and achievement.
-
-Your Certificate of Achievement is attached to this email, acknowledging your outstanding work and dedication.
-
-This certificate is a testament to your commitment to excellence. We are proud of your accomplishments and look forward to your continued success.
-
-Congratulations once again on this well-deserved recognition!
-
-<h4>From the College of Sciences!</h4>''',
-                'is_predefined': True
-            },
-            {
-                'name': 'Template 3 - COG',
-                'subject': 'Copy of Grades',
+                'name': 'Report of Grades',
+                'subject': 'Report of Grades',
                 'header_message': 'Hello, Alchemist!',
                 'body_content': '''Kindly see the attached file for the copy of your report of grades for the semester: FIRST Sem. 2025-2026
 
@@ -49,6 +32,21 @@ Thank you so much.
     This e-mail is confidential. It may also be legally privileged. If you are not the addressee you should not copy, forward, disclose or use any part of it. 
     If you have received this message in error, please delete it and all copies from your system and notify the sender immediately.
 </div>''',
+                'is_predefined': True
+            },
+            {
+                'name': 'Sample Template - COA',
+                'subject': 'Certificate of Achievement',
+                'header_message': 'Outstanding Achievement!',
+                'body_content': '''We are delighted to recognize your exceptional performance and achievement.
+
+Your Certificate of Achievement is attached to this email, acknowledging your outstanding work and dedication.
+
+This certificate is a testament to your commitment to excellence. We are proud of your accomplishments and look forward to your continued success.
+
+Congratulations once again on this well-deserved recognition!
+
+<h4>From the College of Sciences!</h4>''',
                 'is_predefined': True
             },
         ]
